a3047bfa6ae2a5e62c81ed2b1ed052c4c09ef6e3
mapineda Pineda <mapineda1222@gmail.com>
2016-02-24 18:43:01 -0600

    debugging main app, problem with top nav, and general functionality of the app, ie, nothing clicks, cant make new post etc

--- fpv-todo/www/js/app.js
+++ fpv-todo/www/js/app.js
@@ -3,14 +3,36 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', 'LocalStorageModule']);
+angular.module('starter', ['ionic', 'LocalStorageModule'])
 
-app.config(function (localStorageServiceProvider) {
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+
+  });
+})
+
+//config file for angular app
+.config(function (localStorageServiceProvider) {
     localStorageServiceProvider
       .setPrefix('starter');
   });
 
-  app.controller('main', function ($scope, $ionicModal, localStorageService) { //store the entities name in a variable var taskData = 'task';
+//controllers
+.controller('main', function ($scope, $ionicModal, localStorageService) { //store the entities name in a variable var taskData = 'task';
 
   //initialize the tasks scope with empty array
   $scope.tasks = [];
@@ -56,21 +78,3 @@ app.config(function (localStorageServiceProvider) {
        localStorageService.set(taskData, $scope.tasks);
   }
 });
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
