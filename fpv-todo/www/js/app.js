0043072a86473d8db32b2dcd08804a7fc02ac099
mapineda Pineda <mapineda1222@gmail.com>
2016-02-25 13:25:39 -0600

    added task functions

--- fpv-todo/www/js/app.js
+++ fpv-todo/www/js/app.js
@@ -49,33 +49,48 @@ app.controller('main', function($scope, $ionicModal, localStorageService) { //st
       $scope.newTaskModal = modal;
   });
 
-  $scope.getTasks = function () {
-      //fetches task from local storage
-      if (localStorageService.get(taskData)) {
-             $scope.tasks = localStorageService.get(taskData);
-         } else {
-             $scope.tasks = [];
-         }
-  }
-  $scope.createTask = function () {
-      //creates a new task
-      $scope.tasks.push($scope.task);
-          localStorageService.set(taskData, $scope.tasks);
-          $scope.task = {};
-          //close new task modal
-          $scope.newTaskModal.hide();
-  }
-  $scope.removeTask = function () {
-      //removes a task
-      $scope.tasks.splice(index, 1);
-      localStorageService.set(taskData, $scope.tasks);
-  }
-  $scope.completeTask = function () {
-      //updates a task as completed
-      if (index !== -1) {
-       $scope.tasks[index].completed = true;
-      }
+    $scope.getTasks = function () {
+        //fetches task from local storage
+        if (localStorageService.get(taskData)) {
+               $scope.tasks = localStorageService.get(taskData);
+           } else {
+               $scope.tasks = [];
+           }
 
+    };
+    $scope.createTask = function () {
+           //creates a new task
+           $scope.tasks.push($scope.task);
+           localStorageService.set(taskData, $scope.tasks);
+           $scope.task = {};
+
+           //close new task modal
+           $scope.newTaskModal.hide();
+
+       };
+    $scope.removeTask = function (index) {
+       //removes a task
+       $scope.tasks.splice(index, 1);
        localStorageService.set(taskData, $scope.tasks);
-  }
-});
+
+    };
+    $scope.completeTask = function () {
+        //updates a task as completed
+        if (index !== -1) {
+         $scope.tasks[index].completed = true;
+        }
+
+         localStorageService.set(taskData, $scope.tasks);
+    };
+
+    $scope.openTaskModal = function () {
+        $scope.newTaskModal.show();
+
+    };
+
+    $scope.closeTaskModal = function () {
+        $scope.newTaskModal.hide();
+        
+    };
+
+  });
